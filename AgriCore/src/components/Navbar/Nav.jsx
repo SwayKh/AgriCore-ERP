5e8983c543022aa994eab967cc5b9dfda6db6034
Raghav <raghavraghavkumar18@gmail.com>
2025-11-10 02:21:44 +0530

    Add Nav, Sidebar

---
+++ AgriCore/src/components/Navbar/Nav.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Nav() {
+    return (
+        <AppBar
+            position="fixed"
+            sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        >
+            <Toolbar>
+                <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{
+                        flexGrow: 1,
+                        fontSize: { xs: 24, sm: 30, md: 40 } // Responsive font size
+                    }}
+                >
+                    AgriCore
+                </Typography>
+                <Button color="inherit">Login</Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
